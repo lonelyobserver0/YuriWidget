6f34b75c821815bf56c3ef5a2dbceeb5ee9cd28c
lonelyobserver <lcmulaz@gmail.com>
2025-04-11 00:55:46 +0200

    Working

--- script/main.sh
+++ script/main.sh
@@ -2,5 +2,5 @@
 cd ../
 make
 cd build
-./yuriwidget --title niermenuwidget --config-file file://$HOME/.config/yuriwidget/widget.html
+./yuriwidget --title niermenuwidget --config-file file://$HOME/.config/yuriwidget/widget.html --width 1920 --height 1080 --x 0 --y 0
  
--- src/yuriwidget.c
+++ src/yuriwidget.c
@@ -8,14 +8,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
-#include <sys/un.h>
 #include <sys/socket.h>
+#include <sys/un.h>
+
 
 #define SOCKET_PATH "/tmp/yuriwidget.sock"
 #define LOG_FILE "/tmp/yuriwidget.log"
 
-static GtkWidget *main_window = NULL;
-static WebKitWebView *webview = NULL;
+static char *config_url = NULL;
+static char *window_title = "yuriwidget";
+static int win_width = 800;
+static int win_height = 600;
+static int win_x = -1;
+static int win_y = -1;
 
 static void log_message(const char *message) {
     FILE *log_file = fopen(LOG_FILE, "a");
@@ -27,50 +32,24 @@ static void log_message(const char *message) {
 }
 
 static void apply_hyprland_window_settings(GtkWidget *window) {
-    gtk_widget_set_app_paintable(window, TRUE);
-    gtk_widget_realize(window);
-
-    GdkScreen *screen = gtk_widget_get_screen(window);
-    GdkVisual *visual = gdk_screen_get_rgba_visual(screen);
-    if (visual && gdk_screen_is_composited(screen)) {
-        gtk_widget_set_visual(window, visual);
-    }
-
     GdkWindow *gdk_window = gtk_widget_get_window(window);
     if (gdk_window) {
         GdkRGBA transparent = {0, 0, 0, 0};
         gdk_window_set_background_rgba(gdk_window, &transparent);
     }
-
-    gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
 }
 
-typedef struct {
-    gboolean show;
-} WindowCommand;
-
-gboolean handle_window_command(gpointer data) {
-    WindowCommand *cmd = (WindowCommand *)data;
-    if (cmd->show) {
-        gtk_widget_show(main_window);
-    } else {
-        gtk_widget_hide(main_window);
-    }
-    free(cmd);
-    return FALSE;
-}
-
-static void *socket_server_thread(void *arg) {
-    unlink(SOCKET_PATH);
-
+static void socket_server_thread(void *arg) {
     int sockfd;
     struct sockaddr_un server_addr;
 
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd == -1) {
         log_message("Error creating socket");
-        return NULL;
+        return;
     }
+
+    unlink(SOCKET_PATH);
 
     memset(&server_addr, 0, sizeof(struct sockaddr_un));
     server_addr.sun_family = AF_UNIX;
@@ -79,13 +58,13 @@ static void *socket_server_thread(void *arg) {
     if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) == -1) {
         log_message("Error binding socket");
         close(sockfd);
-        return NULL;
+        return;
     }
 
     if (listen(sockfd, 5) == -1) {
         log_message("Error listening on socket");
         close(sockfd);
-        return NULL;
+        return;
     }
 
     log_message("Server listening on socket...");
@@ -103,14 +82,14 @@ static void *socket_server_thread(void *arg) {
             buffer[len] = '\0';
             log_message(buffer);
 
-            if (strncmp(buffer, "show", 4) == 0) {
-                WindowCommand *cmd = malloc(sizeof(WindowCommand));
-                cmd->show = TRUE;
-                g_idle_add(handle_window_command, cmd);
-            } else if (strncmp(buffer, "hide", 4) == 0) {
-                WindowCommand *cmd = malloc(sizeof(WindowCommand));
-                cmd->show = FALSE;
-                g_idle_add(handle_window_command, cmd);
+            if (strncmp(buffer, "show ", 5) == 0) {
+                const char *title = buffer + 5;
+                log_message("Show window with title: ");
+                log_message(title);
+            } else if (strncmp(buffer, "hide ", 5) == 0) {
+                const char *title = buffer + 5;
+                log_message("Hide window with title: ");
+                log_message(title);
             }
         }
 
@@ -118,17 +97,26 @@ static void *socket_server_thread(void *arg) {
     }
 
     close(sockfd);
-    return NULL;
 }
 
-static GtkWidget *create_yuriwidget_window(const char *title, const char *url) {
+static GtkWidget *create_yuriwidget_window() {
+    GtkWidget *main_window;
+    WebKitWebView *webview;
+
     main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(main_window), title);
-    gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 600);
+    gtk_window_set_title(GTK_WINDOW(main_window), window_title);
+    gtk_window_set_default_size(GTK_WINDOW(main_window), win_width, win_height);
+
+    if (win_x >= 0 && win_y >= 0) {
+        gtk_window_move(GTK_WINDOW(main_window), win_x, win_y);
+    }
 
     webview = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    webkit_web_view_load_uri(webview, url);
     gtk_container_add(GTK_CONTAINER(main_window), GTK_WIDGET(webview));
+
+    if (config_url) {
+        webkit_web_view_load_uri(webview, config_url);
+    }
 
     gtk_widget_show_all(main_window);
 
@@ -136,45 +124,50 @@ static GtkWidget *create_yuriwidget_window(const char *title, const char *url) {
 }
 
 static void destroy(GtkWidget *window, gpointer data) {
-    unlink(SOCKET_PATH);
     gtk_main_quit();
 }
 
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
-
-    const char *url = "https://example.com";
-    const char *title = "yuriwidget";
-
+static void parse_args(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"config-file", required_argument, 0, 'c'},
         {"title", required_argument, 0, 't'},
+        {"width", required_argument, 0, 'w'},
+        {"height", required_argument, 0, 'h'},
+        {"x", required_argument, 0, 'x'},
+        {"y", required_argument, 0, 'y'},
         {0, 0, 0, 0}
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "c:t:", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "c:t:w:h:x:y:", long_options, NULL)) != -1) {
         switch (opt) {
-            case 'c':
-                url = optarg;
-                break;
-            case 't':
-                title = optarg;
-                break;
+            case 'c': config_url = g_strdup(optarg); break;
+            case 't': window_title = g_strdup(optarg); break;
+            case 'w': win_width = atoi(optarg); break;
+            case 'h': win_height = atoi(optarg); break;
+            case 'x': win_x = atoi(optarg); break;
+            case 'y': win_y = atoi(optarg); break;
             default:
-                fprintf(stderr, "Usage: %s [-c|--config-file <url>] [-t|--title <window title>]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [--config-file|-c URL] [--title|-t TITLE] [--width|-w N] [--height|-h N] [--x N] [--y N]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    parse_args(argc, argv);
 
     pthread_t socket_thread;
-    pthread_create(&socket_thread, NULL, socket_server_thread, NULL);
+    pthread_create(&socket_thread, NULL, (void *(*)(void *))socket_server_thread, NULL);
 
-    GtkWidget *window = create_yuriwidget_window(title, url);
-    apply_hyprland_window_settings(window);
-    g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
+    GtkWidget *main_window = create_yuriwidget_window();
+    apply_hyprland_window_settings(main_window);
+
+    g_signal_connect(main_window, "destroy", G_CALLBACK(destroy), NULL);
 
     gtk_main();
+
     pthread_join(socket_thread, NULL);
 
     return 0;
