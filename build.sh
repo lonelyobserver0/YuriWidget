8f9d0c5e664fa0a54c3d6a4e052427d34e52536d
lonelyobserver <lcmulaz@gmail.com>
2025-05-10 06:55:15 +0200

    Version: v2 0.0.1

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc yuriwidget.c deps/tomilc99/toml.c -o yuriwidget `pkg-config --cflags --libs gtk4`
+gcc yuriwidget.c deps/tomlc99/toml.c -o yuriwidget `pkg-config --cflags --libs gtk4`
